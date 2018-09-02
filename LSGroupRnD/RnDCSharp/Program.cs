b10f3afa849da281b806c7965e16991a3511640e
ultimatemanas <manaspattanaik777@gmail.com>
2018-09-02 14:55:12 +0530

    Test from ritesh

--- LSGroupRnD/RnDCSharp/Program.cs
+++ LSGroupRnD/RnDCSharp/Program.cs
@@ -11,6 +11,7 @@ namespace RnDCSharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Test");
             Console.ReadLine();
         }
     }
