d1ac6a323bb442ef164f3d11b869b5b670fd2b79
ultimatemanas <manaspattanaik777@gmail.com>
2018-09-05 16:19:07 +0530

    Adde for static member

--- LSGroupRnD/RnDCSharp/Program.cs
+++ LSGroupRnD/RnDCSharp/Program.cs
@@ -6,12 +6,44 @@ using System.Threading.Tasks;
 
 namespace RnDCSharp
 {
-    class Program
+    class ProgramStatis
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            CheckStatic chkObj = new CheckStatic();
+            chkObj.Show();
+            CheckStatic chk = new CheckStatic(5);
+            chk.Show();
+            CheckStatic chk2 = new CheckStatic(6);
+            chk2.Show();
+            chk.ShowStatic();
+            chk2.ShowStatic();
             Console.ReadLine();
+
+        }
+    }
+
+    class CheckStatic
+    {
+        int xNonStatix = 1;
+        static int xStatic = 7;
+        public CheckStatic()
+        {
+
+        }
+
+        public CheckStatic(int num)
+        {
+            this.xNonStatix = num;
+        }
+        public void Show()
+        {
+            Console.WriteLine("X nostatic value is:" + this.xNonStatix);
+        }
+
+        public void ShowStatic()
+        {
+            Console.WriteLine("Static value is:" + CheckStatic.xStatic);
         }
     }
 }
